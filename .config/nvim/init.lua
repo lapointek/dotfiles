142ffbe44f44130e354ba0ba046cb5dcc7bd58b2
lapointek <lapointek.dev@gmail.com>
2025-09-05 21:45:19 -0400

    refactor(init.lua): add vscode conditional loading in neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,10 @@
-require('config.options')
-require('config.keybinds')
-require('config.autocmds')
-require('utils.diagnostics')
-require('config.lazy')
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim
+    require('config.options')
+    require('config.keybinds')
+    require('config.autocmds')
+    require('utils.diagnostics')
+    require('config.lazy')
+end
