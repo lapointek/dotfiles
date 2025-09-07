5079807a9dc3d95f1d2d6a8b11ae0b74749d1f66
lapointek <lapointek.dev@gmail.com>
2025-09-07 18:56:31 -0400

    chore(nvim): add options and keybindings

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Write the contents before a command
+vim.opt.autowrite = true
+-- Write the contents, if it has been modified
+vim.opt.autoread = true
+-- Make a backup before overwriting a file
+vim.opt.backup = false
+-- Make a backup before overwriting a file
+vim.opt.writebackup = true
+-- Whether to use swap files
+vim.opt.swapfile = true
+-- Enable persistent undo
 vim.opt.undofile = true
 -- Directory to store undo file
 vim.opt.undodir = vim.fn.expand("~/.vim/nvim-undo")
