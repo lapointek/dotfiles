99870f8d7db5d9d8ff826e67e6a3c41f280b117b
lapointek <lapointek.dev@gmail.com>
2025-09-12 18:48:17 -0400

    feat(nvim): add harpoon plugin and diagnostic settings

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        virtual_text = false,
+        underline = false,
+        severity_sort = true,
+      },
+    },
+  },
+}
