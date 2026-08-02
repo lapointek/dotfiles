e55a3ca3d258c1156a106a8919e65a9d94fa68f5
lapointek <lapointek.dev@gmail.com>
2026-08-02 13:49:23 -0400

    chore(niri): update config - Added binds - Added window rules - Added spawn-at-startup for kwallet chore(neovim): update config - Updated autocomplete binding and navigation chore: update niri-portals config chore: update .desktop files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -943,7 +943,11 @@ do
       -- <c-k>: Toggle signature help
       --
       -- See `:help blink-cmp-config-keymap` for defining your own keymap
-      preset = 'default',
+      preset = 'enter',
+
+      -- Tab to cycle through completions and navigate snippets
+      ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+      ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -959,6 +963,12 @@ do
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      selection = {
+          -- 'auto_insert' makes the text appear as you cycle (VSCode style)
+          -- 'manual' requires pressing Enter to insert the text
+          preselect = true,
+          auto_insert = true,
+      },
     },
 
     sources = {
