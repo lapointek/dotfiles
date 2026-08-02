1523cf2c85cd7104bd177b206120852e088d6634
lapointek <lapointek.dev@gmail.com>
2026-08-02 13:58:47 -0400

    fix(neovim): fix error in config - Removed selection in autocomplete

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -963,12 +963,6 @@ do
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = { auto_show = false, auto_show_delay_ms = 500 },
-      selection = {
-          -- 'auto_insert' makes the text appear as you cycle (VSCode style)
-          -- 'manual' requires pressing Enter to insert the text
-          preselect = true,
-          auto_insert = true,
-      },
     },
 
     sources = {
